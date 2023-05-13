d9d854b0830d5eb7a968c4514002bdb2b2c6a562
Brian Cha <66922540+briancha5431@users.noreply.github.com>
2023-05-13 15:59:12 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,83 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+///////////////////
+
+const boxes = document.querySelectorAll('.row, .sub-box');
+const tiltAngle = 10; // Adjust this value to control the amount of tilt
+
+function calculateRotation(x, y, box) {
+    const boxRect = box.getBoundingClientRect();
+    const boxCenterX = boxRect.left + boxRect.width / 2;
+    const boxCenterY = boxRect.top + boxRect.height / 2;
+    const deltaX = x - boxCenterX;
+    const deltaY = y - boxCenterY;
+    const angleX = -tiltAngle * (deltaY / (boxRect.height / 2));
+    const angleY = tiltAngle * (deltaX / (boxRect.width / 2));
+    return `rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+}
+
+function tiltBox(event) {
+    boxes.forEach(box => {
+        const boxRect = box.getBoundingClientRect();
+        const isMouseOver = (event.clientX >= boxRect.left && event.clientX <= boxRect.right && event.clientY >= boxRect.top && event.clientY <= boxRect.bottom);
+        if (isMouseOver) {
+            const rotation = calculateRotation(event.clientX, event.clientY, box);
+            box.style.transform = `perspective(1000px) ${rotation} scale3d(1.0, 1.0, 1.0)`;
+        } else {
+            box.style.transform = `perspective(1000px) rotateX(0) rotateY(0) scale3d(1.0, 1.0, 1.0)`;
+        }
+    });
+}
+
+document.addEventListener('mousemove', tiltBox);
+
+///////////////////
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
+
+const sr = ScrollReveal({
+    distance: '45px',
+    duration: 2700,
+    reset: true
+})
+
+sr.reveal('.home-text', { delay: 350, origin: 'left' })
+sr.reveal('.home-img', { delay: 350, origin: 'right' })
+
+sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact', { delay: 200, origin: 'bottom' })
+
+//////////////////
+
+// $(function () {
+//     $("#contact-form").submit(function (event) {
+//         event.preventDefault();
+//         var formData = $(this).serialize();
+//         $.ajax({
+//             type: "POST",
+//             url: "send-email.php",
+//             data: formData,
+//             success: function (response) {
+//                 alert("Your message has been sent.");
+//             },
+//             error: function (response) {
+//                 alert("There was an error sending your message.");
+//             },
+//         });
+//     });
+// });
+// FIX ME
